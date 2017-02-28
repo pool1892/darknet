798868637096b3d0aaf859e210cd14e883434322
pool1892 <pool1892@users.noreply.github.com>
2017-02-28 20:16:21 +0100

    Update voc_label.py

--- scripts/voc_label.py
+++ scripts/voc_label.py
@@ -6,7 +6,7 @@ from os.path import join
 
 sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["full_cart", "empty_cart", "full_basket", "empty_basket"]
 
 
 def convert(size, box):
